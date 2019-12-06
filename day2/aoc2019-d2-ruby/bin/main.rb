eca3964879bef67de9b23a0570ed62db42ae972d
Jeffrey04 <mycyberpet@yahoo.com>
2019-12-07 03:29:57 +0800

    add solution for puzzle 2

--- day2/aoc2019-d2-ruby/bin/main.rb
+++ day2/aoc2019-d2-ruby/bin/main.rb
@@ -4,7 +4,33 @@ def compute_puzzle1()
     intcode_compute(
         File.readlines(ENV['PUZZLE_INPUT']).join(''),
         12,
-        2)
+        2)[0]
 end
 
-puts "Answer for puzzle 1 is #{compute_puzzle1[0]}"
+def pair_search(input, noun=0, verb=0)
+    result = false
+
+    if noun <= 99
+        # do the test
+        memory = intcode_compute(input, noun, verb)
+
+        if memory[0] == 19690720
+            result = 100 * noun + verb
+        else
+            if verb >= 99
+                result = pair_search(input, noun + 1, 0)
+            else
+                result = pair_search(input, noun, verb + 1)
+            end
+        end
+    end
+
+    result
+end
+
+def compute_puzzle2()
+    pair_search(File.readlines(ENV['PUZZLE_INPUT']).join(''))
+end
+
+puts "Answer for puzzle 1 is #{compute_puzzle1}"
+puts "Answer for puzzle 2 is #{compute_puzzle2}"
