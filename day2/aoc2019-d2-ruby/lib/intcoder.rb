742aedfd521d6535f2e2a0d5da21579811b776d3
Jeffrey04 <mycyberpet@yahoo.com>
2019-12-07 02:55:48 +0800

    renamed stuff according to puzzle 2

--- day2/aoc2019-d2-ruby/lib/intcoder.rb
+++ day2/aoc2019-d2-ruby/lib/intcoder.rb
@@ -1,47 +1,47 @@
-def code_ref_value(code, reference_pointer)
-    code[code[reference_pointer]]
-end
-
 def input_parse(input)
     input \
         .split(',')
         .map{|x| x.strip.to_i}
 end
 
-def input_eval(code, pointer=0)
+def memory_ref_value(memory, address)
+    memory[memory[address]]
+end
+
+def memory_eval(memory, address=0)
     result = []
 
-    if code[pointer] == 99
-        result = code
-    elsif code[pointer] == 1
-        result = input_eval(
-            input_eval_addition(code, pointer),
-            pointer + 4)
-    elsif code[pointer] == 2
-        result = input_eval(
-            input_eval_multiplication(code, pointer),
-            pointer + 4)
+    if memory[address] == 99
+        result = memory
+    elsif memory[address] == 1
+        result = memory_eval(
+            memory_eval_addition(memory, address),
+            address + 4)
+    elsif memory[address] == 2
+        result = memory_eval(
+            memory_eval_multiplication(memory, address),
+            address + 4)
     end
 
     result
 end
 
-def input_eval_addition(code, pointer)
-    code.map.with_index{|x, i|
-        i == code[pointer + 3] \
-            ? code_ref_value(code, pointer + 1) + code_ref_value(code, pointer + 2)
+def memory_eval_addition(memory, address)
+    memory.map.with_index{|x, i|
+        i == memory[address + 3] \
+            ? memory_ref_value(memory, address + 1) + memory_ref_value(memory, address + 2)
             : x
     }
 end
 
-def input_eval_multiplication(code, pointer)
-    code.map.with_index{|x, i|
-        i == code[pointer + 3] \
-            ? code_ref_value(code, pointer + 1) * code_ref_value(code, pointer + 2)
+def memory_eval_multiplication(memory, address)
+    memory.map.with_index{|x, i|
+        i == memory[address + 3] \
+            ? memory_ref_value(memory, address + 1) * memory_ref_value(memory, address + 2)
             : x
     }
 end
 
 def intcode_compute(input)
-    input_eval(input_parse(input))
+    memory_eval(input_parse(input))
 end
