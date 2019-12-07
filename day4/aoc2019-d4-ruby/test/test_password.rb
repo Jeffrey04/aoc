c30d34549ed5348802a6916fd3c1619559c0094a
Jeffrey04 <mycyberpet@yahoo.com>
2019-12-07 18:35:59 +0800

    new unit test for part 2

--- day4/aoc2019-d4-ruby/test/test_password.rb
+++ day4/aoc2019-d4-ruby/test/test_password.rb
@@ -7,4 +7,10 @@ class TestIntcoder < Minitest::Test
     assert_equal false, password_is_good('223450')
     assert_equal false, password_is_good('123789')
   end
+
+  def test_part2
+    assert_equal true, password_is_good_too('112233')
+    assert_equal false, password_is_good_too('123444')
+    assert_equal true, password_is_good_too('111122')
+  end
 end
