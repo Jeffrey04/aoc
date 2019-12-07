3c7ab8b3fa70989f5c63489f215c93e8705eedad
Jeffrey04 <mycyberpet@yahoo.com>
2019-12-07 17:33:01 +0800

    part 2 done

--- day3/aoc2019-d3-ruby/src/main.rb
+++ day3/aoc2019-d3-ruby/src/main.rb
@@ -1,7 +1,11 @@
 require 'wiring'
 
 def compute_puzzle1()
-    distance_measure(*File.readlines(ENV['PUZZLE_INPUT']))
+    distance_measure_manhattan(*File.readlines(ENV['PUZZLE_INPUT']))
 end
 
-puts "RUBY: #{compute_puzzle1}"
+def compute_puzzle2()
+    distance_measure_path(*File.readlines(ENV['PUZZLE_INPUT']))
+end
+
+puts "RUBY:\t#{compute_puzzle1}\t#{compute_puzzle2}"
