177998daa7f6006cbaf3feba05a82b0b91c66c4d
Jeffrey04 <mycyberpet@yahoo.com>
2019-12-07 19:33:14 +0800

    solution to the problem

--- day4/aoc2019-d4-ruby/src/password.rb
+++ day4/aoc2019-d4-ruby/src/password.rb
@@ -1,7 +1,34 @@
+require 'enumerator'
+
+def password_adjacent_digits_not_part_of_larger_group(password)
+    password.split('')
+        .each_cons(2)
+        .reduce({}) {|current, incoming|
+            result = current
+
+            if incoming[0] == incoming[1]
+                result[incoming.join()] = result.fetch(incoming.join(), 0) + 1
+            end
+
+            result
+        }.value? 1
+end
+
+def password_get_candidates(checker, min, max)
+    (min.to_i..max.to_i)
+        .select {|candidate| checker.call(candidate.to_s)}
+        .map {|x| x.to_s}
+end
+
 def password_is_good(password)
     password.length == 6 \
         && password_has_identical_adjacent_digits(password) \
         && password_has_increasing_digits(password)
+end
+
+def password_is_good_too(password)
+    password_is_good(password) \
+        && password_adjacent_digits_not_part_of_larger_group(password)
 end
 
 def password_has_identical_adjacent_digits(password)
