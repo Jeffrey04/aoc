0411fff490d850222adcdff3294a3c7505b64e93
Jeffrey04 <mycyberpet@yahoo.com>
2024-12-02 15:27:48 +0800

    standardize environment

---
+++ 2024/init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# create ./dayN
+mkdir -p "day$1"
+
+cd "day$1"
+cp ../day1/Makefile .
+
+# create ./dayN/aoc2022-dN-python
+uv init --app aoc2024-d$1-python --package --python 3.13
+
+cd "aoc2024-d$1-python"
+cp ../../day1/aoc2024-d1-python/Makefile .
+echo "source .venv/bin/activate" > .envrc
+direnv allow .
+uv add --dev pytest pytest-cov ipdb
+
+# back to original folder
+cd ../../
