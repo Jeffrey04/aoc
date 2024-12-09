397e847b00aad1b3989577649eda6e0a7eca3030
Jeffrey04 <mycyberpet@yahoo.com>
2024-12-10 02:00:50 +0800

    switch to faster str as blocks

--- 2024/day9/aoc2024-d9-python/src/aoc2024_d9_python/day9b.py
+++ 2024/day9/aoc2024-d9-python/src/aoc2024_d9_python/day9b.py
@@ -1,11 +1,8 @@
 from functools import reduce
-from itertools import chain, repeat
 from sys import stdin
 
-from toolz import sliding_window
 
-
-def block_move(blocks: tuple[int | None, ...]) -> tuple[int | None, ...]:
+def block_move(blocks: str) -> str:
     return block_swap(
         blocks,
         space_find_first_idx(blocks),
@@ -13,29 +10,21 @@ def block_move(blocks: tuple[int | None, ...]) -> tuple[int | None, ...]:
     )
 
 
-def block_swap(
-    blocks: tuple[int | None, ...], alpha: int, beta: int
-) -> tuple[int | None, ...]:
+def block_swap(blocks: str, alpha: int, beta: int) -> str:
     assert alpha < beta
 
-    return (
-        *blocks[:alpha],
-        blocks[beta],
-        *blocks[alpha + 1 : beta],
-        blocks[alpha],
-        *blocks[beta + 1 :],
-    )
+    return f"{blocks[:alpha]}{blocks[beta]}{blocks[alpha + 1 : beta]}{blocks[alpha]}{blocks[beta + 1 :]}"
 
 
-def check_has_space_between_files(blocks: tuple[int | None, ...]) -> bool:
-    return any(isinstance(item, int) for item in blocks[space_find_first_idx(blocks) :])
+def check_has_space_between_files(blocks: str) -> bool:
+    return any(item.isdigit() for item in blocks[space_find_first_idx(blocks) :])
 
 
-def checksum(blocks: tuple[int | None, ...]) -> int:
-    return sum(idx * item for idx, item in enumerate(blocks) if isinstance(item, int))
+def checksum(blocks: str) -> int:
+    return sum(idx * int(item) for idx, item in enumerate(blocks) if item.isdigit())
 
 
-def compact_block(blocks: tuple[int | None, ...]) -> tuple[int | None, ...]:
+def compact_block(blocks: str) -> str:
     # recursion will be super duper slow
     result = blocks
 
@@ -45,9 +34,7 @@ def compact_block(blocks: tuple[int | None, ...]) -> tuple[int | None, ...]:
     return result
 
 
-def compact_file(
-    disk_map: tuple[int, ...], blocks: tuple[int | None, ...]
-) -> tuple[int | None, ...]:
+def compact_file(disk_map: tuple[int, ...], blocks: str) -> str:
     result = blocks
 
     for file_id in range(len(disk_map) // 2, -1, -1):
@@ -59,23 +46,17 @@ def compact_file(
     return result
 
 
-def file_find_first_idx(blocks: tuple[int | None, ...], file_id: int) -> int:
-    return next(
-        idx
-        for idx, item in enumerate(blocks)
-        if isinstance(item, int) and item == file_id
-    )
+def file_find_first_idx(blocks: str, file_id: int) -> int:
+    return blocks.index(str(file_id))
 
 
-def file_find_last_idx(blocks: tuple[int | None, ...]) -> int:
+def file_find_last_idx(blocks: str) -> int:
     return len(blocks) + next(
-        -1 - idx for idx, item in enumerate(blocks[::-1]) if isinstance(item, int)
+        -1 - idx for idx, item in enumerate(blocks[::-1]) if item.isdigit()
     )
 
 
-def file_move(
-    disk_map: tuple[int, ...], blocks: tuple[int | None, ...], file_id: int
-) -> tuple[int | None, ...]:
+def file_move(disk_map: tuple[int, ...], blocks: str, file_id: int) -> str:
     file_idx = file_find_first_idx(blocks, file_id)
 
     space_idx = space_find_idx_by_length(blocks, disk_map[file_id * 2], file_idx)
@@ -87,14 +68,12 @@ def file_move(
     )
 
 
-def parse(input: str) -> tuple[tuple[int, ...], tuple[int | None, ...]]:
+def parse(input: str) -> tuple[tuple[int, ...], str]:
     disk_map = tuple(int(item) for item in input)
 
-    return disk_map, tuple(
-        chain.from_iterable(
-            repeat(idx // 2 if (idx % 2) == 0 else None, item)
-            for idx, item in enumerate(disk_map)
-        )
+    return disk_map, "".join(
+        (str(idx // 2) if (idx % 2) == 0 else ".") * item
+        for idx, item in enumerate(disk_map)
     )
 
 
@@ -110,28 +89,17 @@ def part2(input: str) -> int:
     return checksum(compact_file(disk_map, blocks))
 
 
-def space_find_first_idx(blocks: tuple[int | None, ...]) -> int:
-    return next(
-        (idx for idx, item in enumerate(blocks) if item is None),
-        len(blocks),
-    )
+def space_find_first_idx(blocks: str) -> int:
+    try:
+        return blocks.index(".")
+    except ValueError:
+        return len(blocks)
 
 
-def space_find_idx_by_length(
-    blocks: tuple[int | None, ...], length: int, max_search: int
-) -> int:
+def space_find_idx_by_length(blocks: str, length: int, max_search: int) -> int:
     first_space_idx = space_find_first_idx(blocks)
-    return first_space_idx + next(
-        idx
-        for idx, window in enumerate(
-            sliding_window(length, blocks[first_space_idx:max_search])
-        )
-        if all(item is None for item in window)
-    )
 
-
-def visualize(blocks: tuple[int | None, ...]) -> str:
-    return "".join(str(item) if isinstance(item, int) else "." for item in blocks)
+    return first_space_idx + blocks[first_space_idx:max_search].index("." * length)
 
 
 def main() -> None:
--- 2024/day9/aoc2024-d9-python/tests/test_day9b.py
+++ 2024/day9/aoc2024-d9-python/tests/test_day9b.py
@@ -9,7 +9,6 @@ from aoc2024_d9_python.day9b import (
     part1,
     part2,
     space_find_idx_by_length,
-    visualize,
 )
 
 input = ("12345", "2333133121414131402")
@@ -20,12 +19,12 @@ def test_parse() -> None:
 
     disk_map, blocks = parse(input[0])
     assert disk_map == (1, 2, 3, 4, 5)
-    assert visualize(blocks) == expected
+    assert (blocks) == expected
 
     expected = "00...111...2...333.44.5555.6666.777.888899"
 
     disk_map, blocks = parse(input[1])
-    assert visualize(blocks) == expected
+    assert (blocks) == expected
 
 
 def test_check_has_space() -> None:
@@ -50,23 +49,23 @@ def test_move_block():
 
     expected = "02.111....2222."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "022111....222.."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "0221112...22..."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "02211122..2...."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "022111222......"
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     with pytest.raises(AssertionError):
         block_move(blocks)
@@ -75,51 +74,51 @@ def test_move_block():
 
     expected = "009..111...2...333.44.5555.6666.777.88889."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "0099.111...2...333.44.5555.6666.777.8888.."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "00998111...2...333.44.5555.6666.777.888..."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "009981118..2...333.44.5555.6666.777.88...."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "0099811188.2...333.44.5555.6666.777.8....."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "009981118882...333.44.5555.6666.777......."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "0099811188827..333.44.5555.6666.77........"
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "00998111888277.333.44.5555.6666.7........."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "009981118882777333.44.5555.6666..........."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "009981118882777333644.5555.666............"
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "00998111888277733364465555.66............."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
     expected = "0099811188827773336446555566.............."
 
-    assert visualize(blocks := block_move(blocks)) == expected
+    assert (blocks := block_move(blocks)) == expected
 
 
 def test_compact_block() -> None:
@@ -127,12 +126,12 @@ def test_compact_block() -> None:
 
     expected = "022111222......"
 
-    assert visualize(compact_block(blocks)) == expected
+    assert (compact_block(blocks)) == expected
 
     _, blocks = parse(input[1])
     expected = "0099811188827773336446555566.............."
 
-    assert visualize(compact_block(blocks)) == expected
+    assert (compact_block(blocks)) == expected
 
 
 def test_part1() -> None:
@@ -156,19 +155,19 @@ def test_move_file() -> None:
 
     expected = "0099.111...2...333.44.5555.6666.777.8888.."
 
-    assert visualize(blocks := file_move(disk_map, blocks, 9)) == expected
+    assert (blocks := file_move(disk_map, blocks, 9)) == expected
 
     expected = "0099.1117772...333.44.5555.6666.....8888.."
 
-    assert visualize(blocks := file_move(disk_map, blocks, 7)) == expected
+    assert (blocks := file_move(disk_map, blocks, 7)) == expected
 
     expected = "0099.111777244.333....5555.6666.....8888.."
 
-    assert visualize(blocks := file_move(disk_map, blocks, 4)) == expected
+    assert (blocks := file_move(disk_map, blocks, 4)) == expected
 
     expected = "00992111777.44.333....5555.6666.....8888.."
 
-    assert visualize(blocks := file_move(disk_map, blocks, 2)) == expected
+    assert (blocks := file_move(disk_map, blocks, 2)) == expected
 
 
 def test_part2() -> None:
