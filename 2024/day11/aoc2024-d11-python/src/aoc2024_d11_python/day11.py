981e7b08c6a302b51458c30404bddad92d3ea5d3
Jeffrey04 <mycyberpet@yahoo.com>
2024-12-11 15:52:43 +0800

    day 11 of 2024

---
+++ 2024/day11/aoc2024-d11-python/src/aoc2024_d11_python/day11.py
@@ -0,0 +1,109 @@
+from functools import reduce
+from itertools import chain
+from math import floor, log10
+from sys import stdin
+from typing import Any, Callable, Generator
+
+from toolz import merge_with, pipe
+
+
+def to_tuple(func) -> Callable[..., Any]:
+    def inner(*arg, **kwargs) -> Any:
+        return (func(*arg, **kwargs),)
+
+    return inner
+
+
+def len_digit(number: int) -> int:
+    return floor(log10(number) + 1)
+
+
+@to_tuple
+def stone_increment(stone: int) -> int:
+    assert stone == 0
+
+    return 1
+
+
+def stone_split(stone: int) -> tuple[int, int]:
+    assert len_digit(stone) % 2 == 0
+
+    return (
+        int(stone / (10 ** (len_digit(stone) / 2))),
+        int(stone % (10 ** (len_digit(stone) / 2))),
+    )
+
+
+@to_tuple
+def stone_multiply(stone: int) -> int:
+    return stone * 2024
+
+
+def stone_transform(stone: int) -> tuple[int, ...]:
+    result = ()
+
+    if stone == 0:
+        result = stone_increment(stone)
+
+    elif len_digit(stone) % 2 == 0:
+        result = stone_split(stone)
+
+    else:
+        result = stone_multiply(stone)
+
+    return result
+
+
+def blink(stones: tuple[int, ...], iterations: int = 1) -> Generator[int, None, None]:
+    return reduce(
+        lambda current, _: chain.from_iterable(
+            stone_transform(stone) for stone in current
+        ),
+        range(iterations),
+        stones,  # type: ignore
+    )
+
+
+def blink_for_count(stones: tuple[int, ...], iterations: int = 1) -> int:
+    return sum(
+        pipe(
+            {stone: 1 for stone in stones},
+            *((blink_get_index,) * iterations),
+        ).values()
+    )
+
+
+def blink_get_index(current: dict[int, int]) -> dict[int, int]:
+    return merge_with(
+        sum,
+        *(
+            {stone: count_current}
+            for stone, count_current in (
+                (stone, count_current)
+                for stone_current, count_current in current.items()
+                for stone in stone_transform(stone_current)
+            )
+        ),
+    )
+
+
+def parse(input: str) -> tuple[int, ...]:
+    return tuple(int(item) for item in input.strip().split(" "))
+
+
+def part1(input: str) -> int:
+    return len(tuple(blink(parse(input), 25)))
+
+
+def part2(input: str) -> int:
+    return blink_for_count(parse(input), 75)
+
+
+def main() -> None:
+    input = stdin.read()
+
+    print("PYTHON:", part1(input), part2(input))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/day11/aoc2024-d11-python/tests/test_day11.py
@@ -0,0 +1,105 @@
+import pytest
+
+from aoc2024_d11_python.day11 import (
+    blink,
+    blink_for_count,
+    part1,
+    stone_increment,
+    stone_multiply,
+    stone_split,
+)
+
+
+def test_stone_increment() -> None:
+    input = 0
+    expected = (1,)
+
+    assert stone_increment(input) == expected
+
+    with pytest.raises(AssertionError):
+        stone_increment(1)
+
+
+def test_stone_split() -> None:
+    input = 1000
+    expected = (10, 0)
+
+    assert stone_split(input) == expected
+
+    with pytest.raises(AssertionError):
+        stone_split(101)
+
+
+def test_stone_multiply() -> None:
+    input = 1
+    expected = (2024,)
+
+    assert stone_multiply(input) == expected
+
+
+def test_blink() -> None:
+    input = (0, 1, 10, 99, 999)
+    expected = (1, 2024, 1, 0, 9, 9, 2021976)
+
+    assert tuple(blink(input)) == expected
+
+    input = (125, 17)
+
+    expected = (253000, 1, 7)
+    assert (input := tuple(blink(input))) == expected
+
+    expected = (253, 0, 2024, 14168)
+    assert (input := tuple(blink(input))) == expected
+
+    expected = (512072, 1, 20, 24, 28676032)
+    assert (input := tuple(blink(input))) == expected
+
+    expected = (512, 72, 2024, 2, 0, 2, 4, 2867, 6032)
+    assert (input := tuple(blink(input))) == expected
+
+    expected = (1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32)
+    assert (input := tuple(blink(input))) == expected
+
+    expected = (
+        2097446912,
+        14168,
+        4048,
+        2,
+        0,
+        2,
+        4,
+        40,
+        48,
+        2024,
+        40,
+        48,
+        80,
+        96,
+        2,
+        8,
+        6,
+        7,
+        6,
+        0,
+        3,
+        2,
+    )
+    assert (input := tuple(blink(input))) == expected
+
+    input2 = (125, 17)
+    expected = 55312
+
+    assert len(tuple(blink(input2, 25))) == expected
+
+def test_blink_for_count():
+    input = (125, 17)
+    expected = 55312
+
+    assert blink_for_count(input, 25) == expected
+
+
+def test_part1() -> None:
+    input = "125 17"
+    expected = 55312
+
+    assert part1(input) == expected
